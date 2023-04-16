6ce1890aaca1508b71327ca0c25d7466af72d6a5
Austin Drummond <adrum0x7@gmail.com>
2023-04-16 15:46:28 -0400

    added a vite helper

---
+++ InertiaCore/Utils/Vite.cs
@@ -0,0 +1,130 @@
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace InertiaCore.Utils;
+
+public static class Vite
+{
+
+    private static string GetString(IHtmlContent content)
+    {
+        var writer = new System.IO.StringWriter();
+        content.WriteTo(writer, HtmlEncoder.Default);
+        return writer.ToString();
+    }
+
+    private static string publicDir(string path)
+    {
+        return "wwwroot/build/" + path;
+    }
+
+    public static HtmlString input(string path)
+    {
+        if (isRunningHot())
+        {
+            return new HtmlString(makeModuleTag(hotAsset("@vite/client")) + makeModuleTag(hotAsset(path)));
+        }
+
+        if (!File.Exists(publicDir("manifest.json")))
+        {
+            throw new Exception("Vite Manifest is missing. Run `npm run build` and try again.");
+        }
+
+        var manifest = File.ReadAllText(publicDir("manifest.json"));
+        var manifestJson = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(manifest);
+
+        if (manifestJson == null)
+        {
+            throw new Exception("Vite Manifest is invalid. Run `npm run build` and try again.");
+        }
+
+        if (!manifestJson.ContainsKey(path))
+        {
+            throw new Exception("Asset not found in manifest: " + path);
+        }
+
+        JsonElement obj = manifestJson[path];
+        var filePath = obj.GetProperty("file");
+
+        var builder = new TagBuilder("script");
+        builder.Attributes.Add("type", "text/javascript");
+
+        var physicalPath = publicDir(filePath.ToString());
+        if (File.Exists(physicalPath))
+        {
+            builder.InnerHtml.AppendHtml(File.ReadAllText(physicalPath));
+        }
+
+        var html = GetString(builder);
+
+        try
+        {
+            var css = obj.GetProperty("css");
+            foreach (JsonElement item in css.EnumerateArray())
+            {
+                var cssBuilder = new TagBuilder("style");
+                cssBuilder.Attributes.Add("type", "text/css");
+                cssBuilder.InnerHtml.AppendHtml(File.ReadAllText(publicDir(item.ToString())));
+                html = html + GetString(cssBuilder);
+            }
+        }
+        catch (Exception)
+        {
+
+        }
+
+        return new HtmlString(html);
+    }
+
+    private static string hotAsset(string path)
+    {
+        var hotfile = publicDir("hot");
+        var hotContents = File.ReadAllText(hotfile);
+
+        return hotContents + "/" + path;
+    }
+
+    private static bool isRunningHot()
+    {
+        return File.Exists(publicDir("hot"));
+    }
+
+    private static string? makeModuleTag(string path)
+    {
+        var builder = new TagBuilder("script");
+        builder.Attributes.Add("type", "module");
+
+
+        builder.Attributes.Add("src", path);
+
+        return new HtmlString(GetString(builder)).Value;
+    }
+
+
+    public static HtmlString reactRefresh()
+    {
+
+        if (!isRunningHot())
+        {
+            return new HtmlString("<!-- no hot -->");
+        }
+
+        var builder = new TagBuilder("script");
+        builder.Attributes.Add("type", "module");
+
+        var inner = String.Format(
+            "import RefreshRuntime from '{0}';", hotAsset("@react-refresh")) +
+            "RefreshRuntime.injectIntoGlobalHook(window);" +
+            "window.$RefreshReg$ = () => { };" +
+            "window.$RefreshSig$ = () => (type) => type;" +
+            "window.__vite_plugin_react_preamble_installed__ = true;";
+
+
+        builder.InnerHtml.AppendHtml(inner);
+
+        return new HtmlString(GetString(builder));
+    }
+}
+
