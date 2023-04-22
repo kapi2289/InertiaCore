dbdd45a0f6a983a145dbcfb22720c22626a16af3
Austin Drummond <adrum0x7@gmail.com>
2023-04-22 14:05:45 -0400

    remove classic assertions

--- InertiaCoreTests/UnitTestVite.cs
+++ InertiaCoreTests/UnitTestVite.cs
@@ -30,7 +30,7 @@ public partial class Tests
             "window.$RefreshSig$ = () => (type) => type;" +
             "window.__vite_plugin_react_preamble_installed__ = true;</script>";
 
-        Assert.AreEqual(htmlResult.ToString(), inner);
+        Assert.That(htmlResult.ToString(), Is.EqualTo(inner));
     }
 
     [Test]
@@ -71,26 +71,26 @@ public partial class Tests
         fileSystem.AddFile(@"/wwwroot/build/manifest.json", new MockFileData("{\"app.tsx\": {\"file\": \"assets/main-19038c6a.js\"}}"));
 
         var result = mock.Object.input("app.tsx");
-        Assert.AreEqual(result.ToString(), "<script src=\"/build/assets/main-19038c6a.js\" type=\"text/javascript\"></script>\n\t");
+        Assert.That(result.ToString(), Is.EqualTo("<script src=\"/build/assets/main-19038c6a.js\" type=\"text/javascript\"></script>\n\t"));
 
         // Basic JS File with CSS import
         fileSystem.AddFile(@"/wwwroot/build/manifest.json", new MockFileData("{\"app.tsx\": {\"file\": \"assets/main.js\",\"css\": [\"assets/index.css\"]}}"));
 
         result = mock.Object.input("app.tsx");
-        Assert.AreEqual(result.ToString(), "<script src=\"/build/assets/main.js\" type=\"text/javascript\"></script>\n\t<link href=\"/build/assets/index.css\" rel=\"stylesheet\" />\n\t");
+        Assert.That(result.ToString(), Is.EqualTo("<script src=\"/build/assets/main.js\" type=\"text/javascript\"></script>\n\t<link href=\"/build/assets/index.css\" rel=\"stylesheet\" />\n\t"));
 
         // Basic CSS file
         fileSystem.AddFile(@"/wwwroot/build/manifest.json", new MockFileData("{\"index.scss\": {\"file\": \"assets/index.css\"}}"));
 
         result = mock.Object.input("index.scss");
-        Assert.AreEqual(result.ToString(), "<link href=\"/build/assets/index.css\" rel=\"stylesheet\" />\n\t");
+        Assert.That(result.ToString(), Is.EqualTo("<link href=\"/build/assets/index.css\" rel=\"stylesheet\" />\n\t"));
 
         // Hot file with css import
         fileSystem.AddFile(@"/wwwroot/build/hot", new MockFileData("http://127.0.0.1:5174"));
 
         result = mock.Object.input("index.scss");
-        Assert.AreEqual(result.ToString(), "<script src=\"http://127.0.0.1:5174/@vite/client\" type=\"module\"></script>\n\t" +
-            "<script src=\"http://127.0.0.1:5174/index.scss\" type=\"module\"></script>\n\t");
+        Assert.That(result.ToString(), Is.EqualTo("<script src=\"http://127.0.0.1:5174/@vite/client\" type=\"module\"></script>\n\t" +
+           "<script src=\"http://127.0.0.1:5174/index.scss\" type=\"module\"></script>\n\t"));
     }
 
 }
