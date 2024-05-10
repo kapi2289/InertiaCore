1d84943bd58505996b2c59aabc325846975517f8
Chuck Waters <chuck.waters@outlook.com>
2024-05-10 11:17:43 -0400

    add async logic to lazy prop

--- InertiaCore/Utils/LazyProp.cs
+++ InertiaCore/Utils/LazyProp.cs
@@ -3,8 +3,15 @@ namespace InertiaCore.Utils;
 public class LazyProp
 {
     private readonly Func<object?> _callback;
+    private readonly bool _isAsync = false;
 
     public LazyProp(Func<object?> callback) => _callback = callback;
 
-    public object? Invoke() => _callback.Invoke();
+    public LazyProp(Func<Task<object?>> callback)
+    {
+        _callback = callback;
+        _isAsync = true;
+    }
+
+    public object? Invoke() => (_isAsync == false) ? _callback.Invoke() : Task.Run(() => _callback.Invoke()).GetAwaiter().GetResult();
 }
